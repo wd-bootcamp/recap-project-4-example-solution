64c12e324aa8ffba4c1c9cb5074e7531bf0a2386
Felix Kirchhoff <f.kirchhoff@proton.me>
2025-11-06 12:59:12 +0100

    add uselocalstoragestate

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
 import ColorForm from "./Components/ColorForm/ColorForm";
-import { useState } from "react";
+import useLocalStorageState from "use-local-storage-state";
 
 function App() {
-  const [colors, setColors] = useState(initialColors);
+  const [colors, setColors] = useLocalStorageState("colors", {
+    defaultValue: initialColors,
+  });
 
   function handleAddColor(data) {
     const newColor = {
